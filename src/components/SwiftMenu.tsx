f7ed7cfa6da5d137e415cedfbb0509864cd0859e
EndOfTime6841 <j96hgj248w@privaterelay.appleid.com>
2026-09-22 07:50:52 +0700

    Update SwiftMenu.tsx

--- src/components/SwiftMenu.tsx
+++ src/components/SwiftMenu.tsx
@@ -1,53 +1,10 @@
-import {
-  Button,
-  FormControl,
-  Link,
-  Radio,
-  RadioGroup,
-  Typography,
-} from "@mui/joy";
+import { Button, Typography } from "@mui/joy";
 import { Toolchain, useIDE } from "../utilities/IDEContext";
-import { useEffect, useState } from "react";
-import { openUrl } from "@tauri-apps/plugin-opener";
 import ErrorIcon from "@mui/icons-material/Error";
-import { invoke } from "@tauri-apps/api/core";
 import { SWIFT_VERSION_PREFIX } from "../utilities/constants";
 
 export default () => {
-  const {
-    selectedToolchain,
-    setSelectedToolchain,
-    toolchains,
-    scanToolchains,
-    locateToolchain,
-    isWindows,
-    hasWSL,
-  } = useIDE();
-
-  const isWindowsReady = !isWindows || hasWSL;
-
-  const [allToolchains, setAllToolchains] = useState<Toolchain[]>([]);
-  useEffect(() => {
-    let loadAllToolchains = async () => {
-      let all: Toolchain[] = [];
-      if (toolchains !== null && toolchains.toolchains) {
-        all = [...toolchains.toolchains];
-      }
-      if (
-        selectedToolchain &&
-        !all.some(
-          (t) => stringifyToolchain(t) === stringifyToolchain(selectedToolchain)
-        ) &&
-        (await invoke("validate_toolchain", {
-          toolchainPath: selectedToolchain.path,
-        }))
-      ) {
-        all.push(selectedToolchain);
-      }
-      setAllToolchains(all);
-    };
-    loadAllToolchains();
-  }, [selectedToolchain, toolchains]);
+  const { selectedToolchain, locateToolchain } = useIDE();
 
   return (
     <div
@@ -72,45 +29,6 @@ export default () => {
           No toolchain selected
         </Typography>
       )}
-      <Typography level="body-sm">
-        {toolchains === null
-          ? "Checking for Swift..."
-          : toolchains.swiftlyInstalled
-          ? `Swiftly Detected: ${toolchains.swiftlyVersion}`
-          : "CrossCode was unable to detect Swiftly."}
-      </Typography>
-      {!isWindowsReady && toolchains !== null && allToolchains.length === 0 && (
-        <Typography level="body-md" color="danger">
-          Install WSL before swift, as you need to install swift inside of WSL.
-        </Typography>
-      )}
-      {isWindowsReady && toolchains !== null && allToolchains.length === 0 && (
-        <Typography level="body-md" color="warning">
-          No Swift toolchains found. You can get one by installing swiftly
-          {isWindows && " in WSL"} and running "
-          <span
-            style={{
-              fontFamily: "monospace",
-            }}
-          >
-            swiftly install {SWIFT_VERSION_PREFIX}
-          </span>
-          " or manually. If you have already done so, but it is not showing up,
-          your toolchain installation may be broken. For help, refer to the{" "}
-          <Link
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              openUrl(
-                "https://github.com/nab138/CrossCode/wiki/Troubleshooting#swift-toolchain-not-detected"
-              );
-            }}
-          >
-            troubleshooting guide
-          </Link>
-          .
-        </Typography>
-      )}
       {selectedToolchain !== null && !isCompatable(selectedToolchain) && (
         <Typography level="body-md" color="danger">
           Your selected toolchain is not compatible. Please select a swift{" "}
@@ -118,46 +36,13 @@ export default () => {
           toolchain.
         </Typography>
       )}
-      {toolchains !== null && allToolchains.length > 0 && (
+      {selectedToolchain !== null && isCompatable(selectedToolchain) && (
         <div>
-          <Typography level="body-md">Select a toolchain:</Typography>
-          <RadioGroup
-            value={stringifyToolchain(selectedToolchain)}
-            sx={{
-              marginTop: "var(--padding-xs)",
-              display: "flex",
-              flexDirection: "column",
-              gap: "var(--padding-md)",
-            }}
-          >
-            {allToolchains.map((toolchain) => (
-              <FormControl key={stringifyToolchain(toolchain)}>
-                <Radio
-                  label={
-                    toolchain.version +
-                    (isCompatable(toolchain) ? "" : " - Not Compatable")
-                  }
-                  disabled={!isCompatable(toolchain) || !isWindowsReady}
-                  value={stringifyToolchain(toolchain)}
-                  variant="outlined"
-                  overlay
-                  onChange={() => setSelectedToolchain(toolchain)}
-                />
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: "var(--padding-xs)",
-                  }}
-                >
-                  <Typography level="body-sm">{toolchain.path}</Typography>
-                  <Typography level="body-sm" color="primary">
-                    {toolchain.isSwiftly ? "(Swiftly)" : "(Manually Installed)"}
-                  </Typography>
-                </div>
-              </FormControl>
-            ))}
-          </RadioGroup>
+          <Typography level="body-md">Selected toolchain:</Typography>
+          <Typography level="body-sm">{selectedToolchain.path}</Typography>
+          <Typography level="body-sm" color="success">
+            Version: {selectedToolchain.version}
+          </Typography>
         </div>
       )}
       <div
@@ -166,38 +51,9 @@ export default () => {
           gap: "var(--padding-md)",
         }}
       >
-        {toolchains?.swiftlyInstalled === false &&
-          selectedToolchain === null && (
-            <Button
-              disabled={!isWindowsReady}
-              variant="soft"
-              onClick={() => {
-                openUrl("https://www.swift.org/install/linux");
-              }}
-            >
-              Download Swift
-            </Button>
-          )}
-        {
-          <Button
-            disabled={!isWindowsReady}
-            variant="soft"
-            onClick={() => {
-              scanToolchains();
-            }}
-          >
-            Scan Again
-          </Button>
-        }
-        {
-          <Button
-            variant="soft"
-            onClick={locateToolchain}
-            disabled={!isWindowsReady}
-          >
-            Locate Existing Toolchain
-          </Button>
-        }
+        <Button variant="soft" onClick={locateToolchain}>
+          Select Toolchain
+        </Button>
       </div>
     </div>
   );
@@ -206,9 +62,4 @@ export default () => {
 export function isCompatable(toolchain: Toolchain | null): boolean {
   if (!toolchain) return false;
   return toolchain.version.startsWith(SWIFT_VERSION_PREFIX);
-}
-
-function stringifyToolchain(toolchain: Toolchain | null): string | null {
-  if (!toolchain) return null;
-  return `${toolchain.path}:${toolchain.version}:${toolchain.isSwiftly}`;
 }

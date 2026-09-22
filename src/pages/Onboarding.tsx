f3278111c596f776367a19d82bceb2c0972e687c
EndOfTime6841 <j96hgj248w@privaterelay.appleid.com>
2026-09-22 07:42:21 +0700

    Update Onboarding.tsx
    Small changes

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -15,6 +15,9 @@ import { invoke } from "@tauri-apps/api/core";
 import { useToast } from "react-toast-plus";
 import { relaunch } from "@tauri-apps/plugin-process";
 import { SWIFT_VERSION_PREFIX } from "../utilities/constants";
+import { open } from "@tauri-apps/plugin-dialog";
+
+
 
 export interface OnboardingProps {}
 
@@ -60,7 +63,7 @@ export default ({}: OnboardingProps) => {
             {version && <Typography level="body-sm">v{version}</Typography>}
           </div>
           <Typography level="body-sm">
-            IDE for iOS Development on Windows and Linux
+            IDE for iOS Development on iOS 
           </Typography>
         </div>
       </div>
@@ -84,7 +87,7 @@ export default ({}: OnboardingProps) => {
             href="#"
             onClick={(e) => {
               e.preventDefault();
-              open("https://github.com/nab138/CrossCode/issues");
+              open("https://github.com/endoftime6841/CrossCode-iOS-");
             }}
           >
             github
